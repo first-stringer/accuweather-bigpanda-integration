65bbbae11ff8a374cf9f31c5e302163efa7468be
Justin Michael Stringer <first-stringer@users.noreply.github.com>
2021-06-03 06:01:03 -0500

    adding property files and code to get properties from them

--- accuweather-app.js
+++ accuweather-app.js
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log("Hello world!");
+const propertiesReader = require('properties-reader');
+
+const configurationPropertiesFileName = process.argv[2];
+console.log('configurationPropertiesFileName=' + configurationPropertiesFileName);
+const privatePropertiesFileName = process.argv[3];
+console.log('privatePropertiesFileName=' + privatePropertiesFileName);
+
+const configurationProperties = propertiesReader(configurationPropertiesFileName);
+const privateProperties = propertiesReader(privatePropertiesFileName);
+
+var samplingInterval_ms = configurationProperties.get('sampling-interval-ms');
+console.log('samplingInterval_ms=' + samplingInterval_ms);
+
+const accuWeatherGetCurrentConditionsURL = configurationProperties.get('accuweather.get-current-conditions-url');
+console.log('accuWeatherGetCurrentConditionsURL=' + accuWeatherGetCurrentConditionsURL);
+
+const accuWeatherAPIKey = privateProperties.get('accuweather.api-key');
+console.log('accuWeatherAPIKey=' + accuWeatherAPIKey);
+
+
