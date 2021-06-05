4e01f94f4e4b3967217ef19588b9636d30bec15b
Justin Michael Stringer <first-stringer@users.noreply.github.com>
2021-06-05 04:23:52 -0500

    fix accuweather integration

--- accuweather-app.js
+++ accuweather-app.js
@@ -4,9 +4,9 @@
 const logger = require('./utilities/logger');
 const propertiesReader = require('properties-reader');
 const fetch = require('node-fetch');
+const { Producer } = require('sqs-producer');
 
 // My code
-const webServiceInvoker = require('./web-service-invoker');
 const WeatherAlertMessage = require('./weather-alert-message');
 const constructWeatherAlertMessage = require('./construct-weather-alert-message');
 
@@ -21,26 +21,10 @@ console.log('accuWeatherGetCurrentConditionsURL=' + accuWeatherGetCurrentConditi
 const accuWeatherAPIKey = process.env.ACCUWEATHER_API_KEY;
 console.log('accuWeatherAPIKey=' + accuWeatherAPIKey);
 
-
-async function fetchWeather(locationKey, apiURL, apiKey) {
-    logger.debug("fetchWeather locationKey=" + locationKey);
-    let completeAPIURL = apiURL + locationKey + "?apikey=" + apiKey;
-    logger.debug('fetchWeather completeAPIURL=' + completeAPIURL);
-
-    return webServiceInvoker.invokeWebService(completeAPIURL).then(JSON.parse)
-
-    //     .then(response => response.ok ? response : (function () { throw response }()))
-    //     .then(response => response.json())
-    //     //.then(jsonResponse => constructMessage(jsonResponse))
-    //     //.then(objectMessage => { console.log(objectMessage); return objectMessage; })
-    //     //.then(JSON.stringify)
-    //     //.then(publishMessage)
-    //     //.catch(err => { logger.error(err); throw Error("Bad response from AccuWeather API!"); });
-    //     //.catch(err => { console.error(err); throw Error("Bad response from AccuWeather API!"); });
-    //     .catch(err => { logger.error(err); throw err; });
-}
-
-
+const producer = Producer.create({
+    queueUrl: 'https://sqs.us-east-2.amazonaws.com/421973658829/ccuweather-bigpanda-integration',
+    region: 'us-east-2'
+});
 
 async function processLocationKeys(locationKeys, apiURL, apiKey) {
     Object.keys(locationKeysJSON).forEach(function (key) {
@@ -52,33 +36,29 @@ async function processLocationKeys(locationKeys, apiURL, apiKey) {
         let completeAPIURL = apiURL + value.LocationKey + "?apikey=" + apiKey;
         logger.debug('processLocationKeys-completeAPIURL=' + completeAPIURL);
 
-        // webServiceInvoker.invokeWebService(completeAPIURL)
-        //     //.then(response => response.ok ? response : (function () { throw response }()))
-        //    // .then(response => response.json())
-        //     //.then(response => console.log('uuu' + response.ok))
-        //     .then(response => response.ok)
-        //     .then(console.log)
-        //     //.then(console.log('xxx'))
-        //     //.then(json => JSON.stringify(json, null, 4))
-        //     //.then(console.log)
-        //     //.then(publishMessage)
-        //     .catch(err => { console.error(err); });
-
+        let messageBody = { id: 'id9', body: "" };
         return fetch(completeAPIURL)
             // response.ok if response.status >= 200 && response.status < 300
             .then(response => response.ok ? response : (function () { throw response }()))
             .then(response => response.json())
-            .then(console.log)
-            //.then(jsonResponse => constructMessage(jsonResponse))
-            //.then(objectMessage => { console.log(objectMessage); return objectMessage; })
-            //.then(JSON.stringify)
-            //.then(publishMessage)
-            //.catch(err => { logger.error(err); throw Error("Bad response from AccuWeather API!"); });
-            //.catch(err => { console.error(err); throw Error("Bad response from AccuWeather API!"); });
-            //.catch(err => { logger.error(err); throw err; });
+            //.then(console.log)
+            //.then(json => producer.send([json]))
+            //.then(json => producer.send("hi"))
+            //.then(json => producer.send(JSON.stringify(json)))
+            //.then(json => function (json) { messageBody.body = "hihihih"; producer.send(messageBody) })
+            //.then(json => { messageBody.body = json; producer.send(messageBody); })
+            //.then(json => { messageBody.body = JSON.stringify(json); producer.send(messageBody); })
+            //.then(response => { messageBody.body = JSON.stringify(response.json()); console.log(messageBody); })
+            //.then(response => { messageBody.body = response.json(); console.log(messageBody); })
+            //.then(response => { messageBody.body = response.json(); console.log(response.json()); })
+            //.then(json => { messageBody.body = json; console.log(json); })
+            //.then(json => { messageBody.body = json; console.log(messageBody); })
+            //.then(json => { messageBody.body = json[0]; console.log(JSON.stringify(messageBody)); })
+            //.then(json => { messageBody.body = JSON.stringify(json[0]); console.log(messageBody); })
+            .then(json => { messageBody.body = JSON.stringify(json[0]); producer.send(messageBody); })
+            //.then(json => { producer.send("lllll"); })
+            //.then(json => producer.send("lllll"))
             .catch(err => { logger.error(err); });
-
-
 
         // TODO: include error handling!
         //TODO check for more than one result, i.e., the array size is greater than 1
@@ -88,9 +68,10 @@ async function processLocationKeys(locationKeys, apiURL, apiKey) {
 
 let intervalCount = 0;
 var locationKeysJSON = require(process.argv[3]);
-setInterval(() => {
-    // Read location keys in from file here so that they can be reloaded after each interval.
-    logger.info('Processing location keys. intervalCount=' + ++intervalCount);
-    processLocationKeys(locationKeysJSON, accuWeatherGetCurrentConditionsURL, accuWeatherAPIKey);
-    logger.info('XXXXXXXXXXXXXX');
-}, samplingInterval_ms);
+// setInterval(() => {
+//     // Read location keys in from file here so that they can be reloaded after each interval.
+//     logger.info('Processing location keys. intervalCount=' + ++intervalCount);
+processLocationKeys(locationKeysJSON, accuWeatherGetCurrentConditionsURL, accuWeatherAPIKey);
+// }, samplingInterval_ms);
+
+
